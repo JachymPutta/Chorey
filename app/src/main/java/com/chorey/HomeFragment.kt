6038a4b0408aad378bd6d14e384c7d2fa36662a3
jachym.putta <jachym.putta@u.yale-nus.edu.sg>
2023-01-01 08:31:27 +0100

    Home fragment, app launches

--- app/src/main/java/com/chorey/HomeFragment.kt
+++ app/src/main/java/com/chorey/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.chorey
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,53 +13,81 @@ import androidx.recyclerview.widget.RecyclerView
 import com.chorey.viewmodel.HomeViewModel
 import com.chorey.data.HomeModel
 import com.chorey.adapter.HomeRecyclerViewAdapter
+import com.chorey.databinding.FragmentHomeBinding
 import com.chorey.util.HomeUtil
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 
 /**
  * TODO: Description
  */
-class HomeFragment : Fragment() {
-    private lateinit var home: HomeModel
+class HomeFragment : Fragment(),
+    EventListener<DocumentSnapshot> {
+    private lateinit var homeRef: DocumentReference
     private lateinit var hrvAdapter: HomeRecyclerViewAdapter
-    private val viewModel: HomeViewModel by activityViewModels()
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var firestore: FirebaseFirestore
     val args: HomeFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.all_chores_recycler)
-
-        //Populating the Home variable
-        if (BuildConfig.DEBUG && args.homeId.toInt() == -1 || viewModel.getHomes() == null) {
-            home = HomeUtil.makeRandomHome(requireContext())
-        } else {
-            viewModel.getHomes()!![args.homeId.toInt()]
-        }
-        hrvAdapter = HomeRecyclerViewAdapter(home.chores)
-        setupRecyclerAdapter(hrvAdapter)
-        recyclerView.adapter = hrvAdapter
-        recyclerView.layoutManager = LinearLayoutManager(view.context)
-
-        // Watch for changes in the list
-        //TODO: This should be the chore list not the home list
-//        viewModel.list.observe(viewLifecycleOwner) {
-//            _ -> hrvAdapter.notifyDataSetChanged()
-//        }
-
-        return view
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        firestore = Firebase.firestore
+
+        homeRef = firestore.collection("homes").document(args.homeId)
+
+        // Inflate the layout for this fragment
+        val recyclerView = view.findViewById<RecyclerView>(R.id.all_chores_recycler)
+
+        // TODO: Adapt the Adapter object to have the on data changed function
+//        hrvAdapter = object : HomeRecyclerViewAdapter(emptyList()) {
+//            override fun onDataChanged() {
+//                if (itemCount == 0) {
+//                    binding.allChoresRecycler.visibility = View.GONE
+//                } else {
+//                    binding.allChoresRecycler.visibility = View.VISIBLE
+//                }
+//            }
+//        }
+        recyclerView.adapter = hrvAdapter
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
     }
 
-    fun setupRecyclerAdapter(hrvAdapter: HomeRecyclerViewAdapter) {
-        hrvAdapter.onItemClick = {
-            TODO("Inflate the detail of the chore")
+    override fun onEvent(value: DocumentSnapshot?, error: FirebaseFirestoreException?) {
+        if (error != null) {
+            Log.w(TAG, "home:onEvent ", error)
+            return
         }
+
+        value?.let {
+            val home = value.toObject<HomeModel>()
+            if (home != null) {
+                onHomeLoaded(home)
+            }
+        }
+    }
+
+    private fun onHomeLoaded(homeModel: HomeModel) {
+        binding.homeName.text = homeModel.homeName
+        //TODO: Bind the rest of the properties
+    }
+
+    companion object {
+        const val TAG = "HomeFragment"
     }
 }
