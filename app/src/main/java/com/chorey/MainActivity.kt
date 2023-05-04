016ccaffa5e5da91568212e402a25246c7c25272
jachym.putta <jachym.putta@u.yale-nus.edu.sg>
2023-05-04 15:46:53 +0800

    AdMob - initial implementation - test id

--- app/src/main/java/com/chorey/MainActivity.kt
+++ app/src/main/java/com/chorey/MainActivity.kt
@@ -10,10 +10,12 @@ import com.chorey.fragments.MenuFragment
 import com.chorey.viewmodel.UserViewModel
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MobileAds.initialize(this) {}
         setContentView(R.layout.activity_main)
     }
 }
